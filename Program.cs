3ebc9cc9cd804a7ba9254ce2f4adc10b9af203e5
ladejesus <luisito.dejesus@gmail.com>
2019-05-19 14:42:45 -0400

    added class to program

--- Program.cs
+++ Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
-
+    
 namespace Superhero
 {
-    class Program
+    class SuperHero
+
     {
-        static void Main(string[] args)
+        public string Name { get; set; }
+        public string LaserEyes { get; set; }
+        public int Power { get; set; }
+
+        public SuperHero(string name, string laserEyes, int power)
+
         {
-            Console.WriteLine("Hello World!");
+            Name = name;
+            LaserEyes = laserEyes;
+            Power = power;
+
         }
+    }
+ namespace SuperHero
+    { 
+        class Program
+        {
+            SuperHero DeJesus = new SuperHero("DeJesus", "green", "Invincible");
+            static void Main(string[] args)
+            {
+                Console.WriteLine("What do people call you?");
+                Console.WriteLine("People call me" + SuperHero);
+            }
+
+            {
+                
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
     }
 }
